659586998461656a716bb53748cc0ef7dc587964
danielmarbach <daniel.marbach@openplace.net>
2019-12-02 20:30:54 +0100

    Object literal improvements

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,24 @@
-function multiply(a: number, b = 25) {
-    return a * b;
+const pizza = {
+    name: 'Pepperoni',
+    price: 15,
+    getName() {
+        return this.name;
+    }
+};
+
+interface pizza {
+    name: string,
+    price: number
 }
 
-console.log(multiply(5));
+const toppings = ['pepperoni'];
+
+const orders = { pizza, toppings };
+
+console.log(orders.pizza.getName());
+
+function createOrder(pizza: pizza, toppings: string[]) {
+    return { pizza, toppings };
+}
+
+console.log(createOrder(pizza, toppings));
