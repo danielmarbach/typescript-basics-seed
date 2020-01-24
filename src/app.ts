5dbd4524ed8ee7b3b3681b2adf7ad4817949443a
danielmarbach <daniel.marbach@openplace.net>
2020-01-24 21:40:18 +0100

    Function Types

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,27 @@
-let pizzaSize: string = 'small';
-
-function selectSize(size: 'small' | 'medium' | 'large'): void {
-    pizzaSize = size;
+// named function
+function sumOrderNamed(price: number, quantity: number): number {
+    return price * quantity;
 }
 
-selectSize('medium');
-
-console.log(`Pizza size: ${pizzaSize}`);
-
-let pizzaSizeNumber: number = 1;
-
-// works also for boolean and enums
-function selectSizeNumber(size: 1 | 2 | 3): void {
-    pizzaSizeNumber = size;
+let sumOrderArrow = (price: number, quantity: number): number => {
+    return price * quantity;
 }
 
-selectSizeNumber(3);
+let sumOrderArrowFunctionTyped: Function;
+sumOrderArrowFunctionTyped = sumOrderArrow;
 
-console.log(`Pizza size: ${pizzaSizeNumber}`);
+// or typed
+
+let sumOrderArrowTyped: (price: number, quanity: number) => number;
+sumOrderArrowTyped = (p, q) => p * q; // short and typed version of sumOrderArrow
+
+// or declaration and implementation together
+let sumOrderArrowTypedWithImplementation: (price: number, quanity: number) => number = (p, q) => p * q;
+
+const sum = sumOrderNamed(25, 2);
+const sumArrow = sumOrderArrow(25, 2);
+const sumArrowTyped = sumOrderArrowTyped(25, 2);
+
+console.log(`Total sum: ${sum}`);
+console.log(`Total sum (arrow): ${sumArrow}`);
+console.log(`Total sum (arrow typed): ${sumArrowTyped}`);
