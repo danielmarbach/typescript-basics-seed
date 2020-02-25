8001ac26678f3fa50da257945de9026870db0bc6
danielmarbach <daniel.marbach@openplace.net>
2020-02-25 17:14:12 +0100

    Interfaces with function types

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 interface Pizza {
     name: string;
     sizes: string[];
+    getAvailableSizes(): string[];
 };
-
-interface Pizzas {
-    data: Pizza[];
-}
 
 let pizza: Pizza;
 
 function createPizza(name: string, sizes: string[]): Pizza {
     return {
         name,
-        sizes
+        sizes,
+        getAvailableSizes() {
+            return this.sizes;
+        }
     };
 }
 
