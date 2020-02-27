789759f57ed992ba72ba9e0feb1af24faff00456
danielmarbach <daniel.marbach@openplace.net>
2020-02-27 21:15:14 +0100

    Indexers

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ interface Pizza extends Sizes {
     name: string;
     toppings?: number;
     getAvailableSizes(): string[];
+    [key: number]: string; // or number, any, can also have multiple indexers or be further down
 };
 
 let pizza: Pizza;
@@ -21,3 +22,4 @@ function createPizza(name: string, sizes: string[]): Pizza {
 }
 
 pizza = createPizza('Pepperoni', ['small', 'medium']);
+pizza[1] = 'xuyz';
