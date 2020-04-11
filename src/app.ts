ee83dd5c41de98fbd87740804d599f3e1c75a10a
danielmarbach <daniel.marbach@openplace.net>
2020-04-11 17:08:39 +0200

    Interfaces implements

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-abstract class Sizes {
+interface SizesInterface {
+    availableSizes: string[]; // can't do get and set
+}
+
+abstract class Sizes implements SizesInterface {
     constructor(protected sizes: string[]) { }
 
     set availableSizes(sizes: string[]) {
@@ -11,11 +15,23 @@ abstract class Sizes {
 }
 
 
-class Pizza extends Sizes {
-    private toppings: string[] = [];
+interface PizzaInterface extends SizesInterface {
+    readonly name: string;
+    toppings: string[];
 
-    constructor(readonly name: string, readonly sizes: string[]) {
+    updateSizes(sizes: string[]): void;
+    addTopping(topping: string): void;
+}
+
+class Pizza extends Sizes implements PizzaInterface {
+    toppings: string[] = [];
+
+    constructor(readonly name: string, sizes: string[]) {
         super(sizes);
+    }
+
+    public updateSizes(sizes: string[]) {
+        this.sizes = sizes;
     }
 
     // by default public
